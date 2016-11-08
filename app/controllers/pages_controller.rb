39fc22b7a7a5d635951feef7ef4cc1a799317a4f
Jesson Go <ogjdne@gmail.com>
2016-11-08 23:16:28 +0000

    updated routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def profile
+  end
+
+  def index
+  end
+
+  def home
+  end
+
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  
+  root 'pages#index'
+  
+  # get 'pages/profile' => 'pages#profile'
+
+  # get 'pages/index' => 'pages#index'
+
+  # get 'pages/home' => 'pages#home'
+
+  # get 'pages/explore' => 'pages#explore'
+
+  get 'profile' => 'pages#profile'
+
+  get 'index' => 'pages#index'
+
+  get 'home' => 'pages#home'
+
+  get 'explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
